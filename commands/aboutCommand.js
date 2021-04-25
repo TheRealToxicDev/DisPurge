dc305f8b2b8405c07c7b3795e6261acdeb5b0fac
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 16:56:40 -0600

    Update aboutCommand.js

--- commands/aboutCommand.js
+++ commands/aboutCommand.js
@@ -7,7 +7,7 @@ module.exports.run = (client , message, args) => {
  let embed = new MessageEmbed()
    embed.setAuthor('A bit about me', EmbedConfig.image)
    embed.setColor(EmbedConfig.blurple)
-   embed.setDescription('DisPurge is a About Simple bot written in Discord.js to allow better control over Purging Messages in a Discord Server.')
+   embed.setDescription('DisPurge is a simple bot written in Discord.js to allow better control over Purging Messages in a Discord Server.')
    embed.addField('Developer(s)', 'Towoxic Dev#4200 | PrettyChillDev#0420', true)
    embed.addField('Created On', moment(client.createdAt).format('MM/DD/YYYY HH:mm:ss A'), true)
    embed.setFooter(EmbedConfig.footer, EmbedConfig.image)
