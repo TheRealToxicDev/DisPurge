10208b441884188b4341c6eb74b5b10750b0ab46
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 16:32:44 -0600

    Update serverSchema.js

--- mongo/serverSchema.js
+++ mongo/serverSchema.js
@@ -2,8 +2,8 @@ const mongo = require ('mongoose');
 
 const servers = mongo.Schema({
     ServerID: String,
-    ServerPrefix: String,
-    AuditLogs: String
+    ServerPrefix: { Type: String, default: 'dp.' },
+    AuditLogs: { Type: String, default: 'purge-logs' }
 });
 
 module.exports = mongo.model("servers", servers);
