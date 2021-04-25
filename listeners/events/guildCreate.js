8c72c77ae733fe5159b12015f09c055cfb035950
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 16:29:34 -0600

    Update guildCreate.js

--- listeners/events/guildCreate.js
+++ listeners/events/guildCreate.js
@@ -6,7 +6,7 @@ module.exports = async (client, guild) => {
     const IntializeDB = new ServersDB({
         ServerID: guild.id,
         ServerPrefix: 'dp.',
-        AuditLogs: { Type: String, default: 'purge-logs' }
+        AuditLogs: 'purge-logs'
     });
 
     IntializeDB.save()
