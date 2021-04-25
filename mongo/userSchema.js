4bf471c7d3005ae1b7b70d6800132973e273cb18
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 16:38:39 -0600

    Update userSchema.js

--- mongo/userSchema.js
+++ mongo/userSchema.js
@@ -2,6 +2,6 @@ const { Schema, model } = require ('mongoose');
 
 module.exports = model("users", new Schema({
     UserID: { type: String, default: '00000000000000' },
-    Username: { Type: String, default: 'DefaultUser' },
-    LastPurge: { Type: Map, default: { MessageChannel: 'User has not Purged any Messages', Amount: 0 }}
+    Username: { type: String, default: 'DefaultUser' },
+    LastPurge: { type: Map, default: { MessageChannel: 'User has not Purged any Messages', Amount: 0 }}
 }));
