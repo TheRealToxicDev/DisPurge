6f907e35bbf8cb9b79c71a30a5a986ae524165e0
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 23:50:54 -0600

    Update purge10Command.js

--- commands/purge10Command.js
+++ commands/purge10Command.js
@@ -26,7 +26,7 @@ module.exports.run = async (client , message, args) => {
       message.channel.send(success)
 
     ServerDB.findOne({
-        ServerID: message.guild.id}, (err, res) => {
+        ServerID: message.guild.id}, async (err, res) => {
             
             let channel = message.guild.channels.cache.get(res.AuditLogs)
        
