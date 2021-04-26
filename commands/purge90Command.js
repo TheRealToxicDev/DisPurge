4bc8c9727cc12915d3e1545d594ad39ea564a74f
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-26 00:35:00 -0600

    Create purge90Command.js

---
+++ commands/purge90Command.js
@@ -0,0 +1,73 @@
+const { MessageEmbed } = require ('discord.js');
+const ServerDB = require ('../mongo/serverSchema');
+const UsersDB = require ('../mongo/userSchema');
+const EmbedConfig = require ('../listeners/embeds/main');
+const ErrorEmbedConfig = require ('../listeners/embeds/errors');
+
+module.exports.run = async (client , message, args) => {
+
+    await message.delete().catch(() => {})
+
+    let user = await UsersDB.findOne({ UserID: message.author.id })
+
+    if (!user) await new UsersDB({
+        UserID: message.author.id,
+        Username: message.author.username
+    });
+
+    await message.channel.bulkDelete('90').catch(() => {})
+
+    let success = new MessageEmbed()
+      success.setAuthor('Purge Complete', EmbedConfig.image)
+      success.setColor(EmbedConfig.blurple)
+      success.setDescription('I have purged `90` messages from this channel.')
+      success.setFooter(EmbedConfig.footer, EmbedConfig.image)
+
+      message.channel.send(success)
+
+    ServerDB.findOne({
+        ServerID: message.guild.id}, async (err, res) => {
+            
+            let channel = message.guild.channels.cache.get(res.AuditLogs)
+       
+            if(channel){
+
+                let logs = new MessageEmbed()
+                   logs.setAuthor('Action: Purge', EmbedConfig.image)
+                   logs.setColor(EmbedConfig.blurple)
+                   logs.addField('Mod', `${message.author.username}`, true)
+                   logs.addField('Amount', '`90`', true)
+                   logs.setFooter(EmbedConfig.footer, EmbedConfig.image)
+        
+                await channel.send(logs)
+       }
+   });
+
+   await UsersDB.updateOne({
+       UserID: message.author.id,
+       Username: message.author.username,
+       LastPurge: {
+           MessageChannel: message.channel.name,
+           Amount: '90'
+       }
+   })
+}
+
+module.exports.help = {
+    name: "purge90",
+    category: "purging",
+    aliases: ['p90'],
+    description: "Bulk Delete 90 messages under 14 days old!",
+    example: "``purge50``"
+}
+
+module.exports.requirements = {
+    userPerms: ["MANAGE_MESSAGES"],
+    clientPerms: ["MANAGE_MESSAGES"],
+    ownerOnly: false
+}
+
+module.exports.limits = {
+    rateLimit: 2,
+    cooldown: 1e4
+}
