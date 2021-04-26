5fa9eea962f8b500a98d81b0d3ea939e0bd44eef
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-04-25 18:03:54 -0600

    Update inviteCommand.js

--- commands/inviteCommand.js
+++ commands/inviteCommand.js
@@ -7,8 +7,8 @@ module.exports.run = (client , message, args) => {
    embed.setAuthor('Woah, I feel honored!!', EmbedConfig.image)
    embed.setColor(EmbedConfig.blurple)
    embed.setDescription('You can invite me using the links below')
-   embed.addField('Invite HyperLink', '[Click Here Boii](https://discord.com/api/oauth2/authorize?client_id=835997853263462461&permissions=2617638135&scope=bot)', true)
-   embed.addField('Invite Link (Mobile Users)', 'https://discord.com/api/oauth2/authorize?client_id=835997853263462461&permissions=2617638135&scope=bot', true)
+   embed.addField('Invite HyperLink', '[Click Here Boii](https://api.toxicdev.me/bots/dispurge/inv)', true)
+   embed.addField('Invite Link (Mobile Users)', 'https://api.toxicdev.me/bots/dispurge/inv', true)
    embed.setFooter(EmbedConfig.footer, EmbedConfig.image)
 
    message.channel.send(embed)
