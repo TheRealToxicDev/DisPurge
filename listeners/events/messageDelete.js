597cf1224a3203fcab47633932d20c687c8893a0
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-05-05 19:49:09 -0600

    Update messageDelete.js

--- listeners/events/messageDelete.js
+++ listeners/events/messageDelete.js
@@ -1,16 +1,15 @@
-const moment = require('moment')
+const moment = require('moment');
 
 module.exports = (client, message) => {
-if (client.snipe.has(message.guild.id)) {
-client.snipe.delete(message.guild.id);
-}
-client.snipe.add(message.guild.id);
-client.snipe[message.guild.id] = 
-{
-message: message.content,
-name: message.author.tag,
-channel: message.channel.name,
-time: moment(message.createdAt).format('YYYY-MM-DD'),
-image:message.attachments.first() ? message.attachments.first().proxyURL : null
+
+if (client.snipe.has(message.guild.id)) client.snipe.delete(message.guild.id); //Check if there are any other recent messages
+client.snipe.add(message.guild.id); //Add the guild
+
+client.snipe[message.guild.id] = {
+  message: message.content,
+  name: message.author.tag,
+  channel: message.channel.name,
+  time: moment(message.createdAt).format('YYYY-MM-DD'),
+  image: message.attachments.first() ? message.attachments.first().proxyURL : null
+  };
 };
-}
