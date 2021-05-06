32c27f2b8050800ff57913524ba5607147cdff4d
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-05-05 18:49:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Change this to 'TOken_Here' if you want
-exports.token=process.env.TOKEN
+exports.token='ODM1OTk3ODUzMjYzNDYyNDYx.YIXlrA.wg0YBOEN0FuThfhazyyBmk1GdU8'
 
 /** THIS IS A LIST OF IDS FOR PEOPLE YOU TRUST WITH TTHINGS LIKE THE EVAL COMMAND */
 exports.owners= [
@@ -9,7 +9,9 @@ exports.owners= [
 ]
 
 /** MONGO DB CONNECTION STRING */
-exports.mongo_url=process.env.MONGO_DB
+exports.mongo_url='mongodb://mongo:CSPpLeZAQeHHFFVrEsrc@containers-us-west-5.railway.app:7680'
+exports.MONGO_DB='mongodb://mongo:CSPpLeZAQeHHFFVrEsrc@containers-us-west-5.railway.app:7680'
 
 exports.infinity_votes='https://infinitybotlist.com/bots/835997853263462461/vote'
 exports.fates_votes='https://fateslist.xyz/bot/835997853263462461/vote'
+exports.IBL_AUTH='VRBfPyS4iGETV8JQqbFEmRNkWSZvkbjjCSTjlywuyH1LuMgHfKGMvcPNkIY5Ucflcyfq11v2TlafKkEzN6468IuwXXgPekdPUp8n'
