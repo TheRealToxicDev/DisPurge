3c09cff5d1f3d907480bb44b2d72ca485c8b41d1
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-05-05 19:50:30 -0600

    Update snipeCommand.js

--- commands/snipeCommand.js
+++ commands/snipeCommand.js
@@ -3,30 +3,19 @@ const { MessageEmbed } = require("discord.js")
 module.exports.run = (client , message, args) => {
 
 if (client.snipe.has(message.guild.id)) {
+   let embed = new MessageEmbed()
+   .setAuthor("Deleted Messages", client.user.displayAvatarURL({dynamic: true}));
 
-    let embed = new MessageEmbed()
-      embed.setAuthor("Deleted Messages", client.user.displayAvatarURL({dynamic: true}))
+   if(client.snipe[message.guild.id].image) embed.setImage(client.snipe[message.guild.id].image);
 
-    if(client.snipe[message.guild.id].message){
-        embed.addField(`Snipe: `, `\`\`\`javascript\n${client.snipe[message.guild.id].message}\n\`\`\``)
-    }
+   embed.addField(`Snipe: `, `\`\`\`js\n${client.snipe[message.guild.id].message || "No Content"}\n\`\`\``)
+   embed.addField(`Message Sender: `, client.snipe[message.guild.id].name)
+   embed.addField(`Channel: `, client.snipe[message.guild.id].channel)
+   embed.addField("Sniped By:", `${message.author.tag}`)
+   embed.setFooter("Deleted at: " + client.snipe[message.guild.id].time)
 
-      embed.addField(`Message Sender: `, client.snipe[message.guild.id].name)
-      embed.addField(`Channel: `, client.snipe[message.guild.id].channel)
-      embed.addField("Sniped By:", `${message.author.tag}`)
-      embed.setFooter("Deleted at: " + client.snipe[message.guild.id].time)
-
-    if(client.snipe[message.guild.id].image){
-        embed.setImage(client.snipe[message.guild.id].image)
-    }
-
-    return message.channel.send(embed);
-  }
-
-  let embed1 = new MessageEmbed()
-   embed1.setDescription("``❌ No deleted messages``")
-   
-   return message.channel.send(embed1)
+   message.channel.send(embed);
+  }else message.channel.send(new MessageEmbed().setDescription("``❌ No deleted messages``"))
 }
 
 module.exports.help = {
